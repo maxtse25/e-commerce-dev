1e76cf130ad4e778fc13026f6af777c47eb46a64
maxtse25 <tsechunghei97@outlook.com>
2024-06-18 13:00:07 +0800

    Install jest-environment-jsdom and update dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  setupFilesAfterEnv: [require.resolve('@testing-library/jest-dom/extend-expect')],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
   },
   moduleNameMapper: {
     '^@/components/(.*)$': '<rootDir>/components/$1',
     '^@/lib/(.*)$': '<rootDir>/lib/$1'
-  },
-  // Add other configurations as necessary
+  }
 };
