3a4aadfcd9e2badd24f3c712dbe62fbfc4d257d0
maxtse25 <tsechunghei97@outlook.com>
2024-06-22 22:50:31 +0800

    Add customers' comment section in the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -109,12 +109,65 @@ export default function Home() {
     {/* value proposition section */}
     <section className="bg-slate-100 py-24">
       <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
-          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-5">
             <h2 className="order-1 mt-2 tracking-tight text-center text-balance 
             !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
               What Our <span className="relative px-2">Customers<Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" /></span>{''} Say
             </h2>
             <img src="snake-2.png" className="w-24 order-0 lg:order-2" />
+          </div>
+
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <div className='flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20'>
+              <div className="flex gap-0.5 mb-2">
+                <StarIcon className='h-5 w-5 text-green-600 fill-green-600' />
+                <StarIcon className='h-5 w-5 text-green-600 fill-green-600' />
+                <StarIcon className='h-5 w-5 text-green-600 fill-green-600' />
+                <StarIcon className='h-5 w-5 text-green-600 fill-green-600' />
+                <StarIcon className='h-5 w-5 text-green-600 fill-green-600' />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "The case feels durable and I even got a complement on the design. Had the case for two and a half months now and the images is super clear, on the case I had before, <span className="p-0.5 bg-slate-800 text-white">the image is super clear</span>, on the case I had before, the image started fading into yellow-ish color after a couple weeks. Love it. "
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img className="rounded-full h-12 w-12 object-cover" src="/users/user-1.png" alt="user" />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Max</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <CheckIcon className="h-4 w-4 stroke-[3px] text-green-600"></CheckIcon>
+                    <p className="text-sm">Verfied Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <div className='flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20'>
+              <div className="flex gap-0.5 mb-2">
+                <StarIcon className='h-5 w-5 text-green-600 fill-green-600' />
+                <StarIcon className='h-5 w-5 text-green-600 fill-green-600' />
+                <StarIcon className='h-5 w-5 text-green-600 fill-green-600' />
+                <StarIcon className='h-5 w-5 text-green-600 fill-green-600' />
+                <StarIcon className='h-5 w-5 text-green-600 fill-green-600' />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "The case feels durable and I even got a complement on the design. Had the case for two and a half months now and the images is super clear, on the case I had before, <span className="p-0.5 bg-slate-800 text-white">the image is super clear</span>, on the case I had before, the image started fading into yellow-ish color after a couple weeks. Love it. "
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img className="rounded-full h-12 w-12 object-cover" src="/users/user-2.png" alt="user" />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Zoe</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <CheckIcon className="h-4 w-4 stroke-[3px] text-green-600"></CheckIcon>
+                    <p className="text-sm">Verfied Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
       </MaxWidthWrapper>
     </section>
