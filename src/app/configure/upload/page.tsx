7e7a12d9ebfc1e5bf36611fb05497d3d4aef7dc9
maxtse25 <tsechunghei97@outlook.com>
2024-06-25 13:24:17 +0800

    Update file uploading functionalities: onDropAccepted/Rejected and Add toast

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -1,22 +1,48 @@
 "use client"
 
 import { Progress } from "@/components/ui/progress"
+import { useToast } from "@/components/ui/use-toast"
+import { useUploadThing } from "@/lib/uploadthing"
 import { cn } from "@/lib/utils"
 import { Divide, Image, Loader2, MousePointer2Icon } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 import { useState, useTransition } from "react"
 import Dropzone, { FileRejection } from 'react-dropzone'
 
 const Page = () => {
-
+    const { toast } = useToast()
     const [isDragOver, setIsDragOver] = useState<boolean>(false)
-    const [uploadProgress, setUploadProgress] = useState<number>(45)
+    const [uploadProgress, setUploadProgress] = useState<number>(0)
+    const router = useRouter()
 
-    const onDropRejected = () => {}
-    const onDropAccepted = () => {
-        console.log("accepted")
+    const { startUpload } = useUploadThing("imageUploader", {
+        onClientUploadComplete: ([data]) => {
+            const configId = data.serverData.configId
+            startTransition(() => {
+                router.push(`/configure/design?id=${configId}`)
+            })
+        },
+        onUploadProgress(p) {
+            setUploadProgress(p)
+        },
+    })
+
+    const onDropRejected = (rejectedFiles: FileRejection[]) => {
+        const [file] = rejectedFiles
+        setIsDragOver(false)
+        toast({
+            title: `${file.file.type} type is not supported.`,
+            description: 'Please choose a PNG, JPG or JPEG image instead.',
+            variant: 'destructive'
+        })
+
+    }
+    const onDropAccepted = (acceptedFiles: File[]) => {
+        startUpload(acceptedFiles, { configId: undefined })
+        setIsDragOver(false)
     }
 
-    const isUploading = true
+    const isUploading = false
     const [isPending, startTransition] = useTransition()
 
     return (
@@ -57,12 +83,24 @@ const Page = () => {
                                             className="mt-2 w-40 h-2 bg-gray-300"/>
                                     </div>
                                     ) : isPending ? (
-                                    <div></div>
+                                    <div className="flex flex-col items-center">
+                                        <p>Redirecting, please wait...</p>
+                                    </div>
                                     ) : isDragOver ? (
-                                    <span></span>) : 
-                                    (<span></span>
+                                        <p>
+                                            <span className="font-semibold">Drop file</span>{' '}
+                                            to upload
+                                        </p>
+                                    ) : (
+                                        <p>
+                                            <span className="font-semibold">Click to upload</span>{' '}
+                                            or drag and drop
+                                        </p>
                                     )}
                             </div>
+                            {isPending? null : (
+                                <p className="text-xs text-zinc-500">PNG, JPG, JPEG</p>
+                            )}
                         </div>
                         )}
                 </Dropzone>
